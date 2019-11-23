cbb785d59a18141f1bf05024277affab3a3e0929
Joan Miquel Torres <joanmi@gmail.com>
2019-11-23 17:28:31 +0100

    Webpack config, plugins and loaders.

--- README.md
+++ README.md
@@ -17,3 +17,34 @@ Setup Process
 
     npm install --save-dev webpack webpack-cli
 
+
+### Plugins installation
+
+    npm install --save-dev \
+        html-webpack-plugin \
+        clean-webpack-plugin \
+    ;
+
+
+### Loaders (and dependencies) installation
+
+
+#### To render (s)css
+
+    npm install --save-dev \
+        style-loader \
+        css-loader \
+        sass-loader \
+        node-sass \
+    ;
+
+
+#### To compile PUG templates
+
+    npm install --save-dev pug-loader;
+
+
+#### To require raw assets
+
+    npm install --save-dev file-loader;
+
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+"use strict";
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: {
+    main: './client/main/main.js',
+  },
+  mode: "development",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          'pug-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+};
+
