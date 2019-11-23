307733855ede117be64acc281aa51dbb625519c3
Joan Miquel Torres <joanmi@gmail.com>
2019-11-23 18:04:07 +0100

    Basic Express - Webpack client integration.

--- app.js
+++ app.js
@@ -5,22 +5,25 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'client/main'));
+  // Old views directory moved to client/main
+  // There will be the only views directly served by express
+  // In order to allow error pages to be rendered.
+  // Rest of the application will be generated from Webpack
+  // and statically served.
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
