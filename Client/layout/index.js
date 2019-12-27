904d9b1d67e9db0f02a240c4a4ca24c22636306e
Joan Miquel Torres <jmtorres@112ib.com>
2019-12-27 17:21:28 +0100

    Webpack pug template through html-webpack-pug-plugin

--- Client/layout/index.js
+++ Client/layout/index.js
@@ -1,14 +1,9 @@
 // client/main/layout.js
 // =====================
 import $ from 'jquery';
-import layoutTpl from './layout.pug';
-import mainModel from '@models/main.js';
 
 // Setup layout:
 export const body = $("body");
-body.html(
-    layoutTpl(mainModel)
-);
 
 // App container:
 export const app = $("#app");
