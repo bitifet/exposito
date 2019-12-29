71d8fcc6e3dfa8695f18a5ba96fcf4c7a9ad084d
Joan Miquel Torres <joanmi@gmail.com>
2019-12-29 19:52:04 +0100

    Minor changes.
    (Moving things forth and back... ;-))

--- Client/main/main.js
+++ Client/main/main.js
@@ -3,9 +3,20 @@
 
 export default new Promise(async function(resolve, reject) {
 
-    const Router = await (await require('@client/router'));
-    ///console.log(Router);
+    const {router} = await (await require('@client/router'));
+    const routes = await require('@client/routes');
+    const {app} = require('@client/layout');
 
+
+    const mainRouter = await router(
+        routes
+        , window
+        , app
+    );
+
+    // console.log({
+    //     mainRouter,
+    // });
 
 });
 
--- Client/router/index.js
+++ Client/router/index.js
@@ -1,18 +1,15 @@
-// client/router/index.js
+// Client/router/index.js
 // ======================
 
 module.exports = Promise.resolve().then(async function(){
 
-    const {app} = require('@client/layout');
+    const router = await require('./router');
 
-    const routes = await require('@client/routes');
-    const Router = await require('./router');
-    const mainRouter = await Router(
-        routes
-        , window
-        , app
-    );
-
-    return mainRouter;
+    return {
+        router,
+        // TODO:
+        // -----
+        // window_fake (TODO) will allow alternative routers detached from real window
+    };
 
 });
